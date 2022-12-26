88c5496f8c28ecc272bd43ffccac36d98ef39e20
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-26 04:37:44 -0800

    setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+/* eslint-disable new-cap */
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Lexend, Athiti } from "@next/font/google";
+
+const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" });
+const athiti = Athiti({
+  subsets: ["latin"],
+  variable: "--font-athiti",
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${lexend.variable} ${athiti.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["var(--font-lexend)"],
+        athiti: ["var(--font-athiti)"],
+      },
+      colors: {
+        acm: {
+          blue: "#82aaff",
+          lightpurple: "#c792ea",
+          yellow: "#ffd700",
+          green: "#c3e88d",
+          red: "#ff5370",
+          gray: "#8f93a2",
+          pink: "#f07178",
+          lightblue: "#89ddff",
+          darkgray: "#525975",
+          orange: "#f78c6c",
+          marine: "#80cbc4",
+          purple: "#da70d6",
+          white: "#ffffff",
+          black: "#000000",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
