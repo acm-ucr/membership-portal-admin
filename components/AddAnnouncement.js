b69241f210a04bc1c7878f0772debdc41d4bd4e9
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-26 04:57:22 -0800

    setup issues

---
+++ components/AddAnnouncement.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddAnnouncement = () => {
+  return <div>AddAnnouncement</div>;
+};
+
+export default AddAnnouncement;
---
+++ components/AddResource.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddResource = () => {
+  return <div>AddResource</div>;
+};
+
+export default AddResource;
