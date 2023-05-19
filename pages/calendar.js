bf75d03a2b5cbfa88d1afbb8a4c09d834d022d55
achen409 <andrewr.chen@gmail.com>
2023-05-19 16:11:37 -0700

    Added calendar page

---
+++ pages/calendar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import Calendar from "../components/Calendar";
+
+const calendar = () => {
+  return (
+    <div className="flex justify-start items-center flex-col w-full mt-24 bg-black min-h-screen">
+      <Header title="Calendar" color="bg-acm-black" />
+    </div>
+  );
+};
+
+export default calendar;
