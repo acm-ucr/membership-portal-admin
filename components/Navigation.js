69f9e4220dd765935ff7c713df8db142aedc119b
Bryce7832 <bryce.hojo@gmail.com>
2023-07-04 14:02:28 -0700

    Fixed format

--- components/Navigation.js
+++ components/Navigation.js
@@ -49,8 +49,8 @@ const Navigation = () => {
                 Resume
               </p>
             </Link>
-            </Nav>
-            <Nav className="text-2xl mx-2 flex justify-center items-center">
+          </Nav>
+          <Nav className="text-2xl mx-2 flex justify-center items-center">
             <Link href="/lockers" className="no-underline">
               <p className=" mb-0 text-acm-white font-medium hover:!text-acm-blue">
                 Lockers
